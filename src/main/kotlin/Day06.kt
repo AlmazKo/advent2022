de1eaf2458b43c5d5c820809af4d0240dfb1aca4
Alex Suslov <sys@covesting.io>
2022-12-11 13:35:20 +0300

    day 6 part two

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -4,15 +4,18 @@ object Day06 : Task {
     fun main(args: Array<String>) = execute()
 
     override fun part1(input: Iterable<String>): Any {
-        val index = input.first().toCharArray().toList()
-            .windowed(4, 1)
-            .indexOfFirst { it.distinct().size == 4 }
-        return index + 4
+        return find(input.first(), 4)
     }
 
     override fun part2(input: Iterable<String>): Any {
-        return super.part2(input)
+        return find(input.first(), 14)
     }
 
 
+    private fun find(input: String, windowSize: Int): Int {
+        val index = input.toCharArray().toList()
+            .windowed(windowSize, 1)
+            .indexOfFirst { it.distinct().size == windowSize }
+        return index + windowSize
+    }
 }
