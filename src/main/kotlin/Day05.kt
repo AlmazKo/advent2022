675d88bc0a34751dc21e35af1a0ebf0de9f062a7
Alex Suslov <sys@covesting.io>
2022-12-13 09:38:30 +0300

    day 5 part two

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,5 +1,6 @@
 import java.util.Deque
 import java.util.LinkedList
+import java.util.SortedMap
 import java.util.TreeMap
 
 object Day05 : Task {
@@ -8,36 +9,33 @@ object Day05 : Task {
     fun main(args: Array<String>) = execute()
 
     override fun part1(input: Iterable<String>): Any {
-        val crates = TreeMap<Int, Deque<Char>>()
-        input.forEach {
-            if (it.startsWith('m')) {
-                val cmd = it.parseCommand()
-                val from = crates[cmd.from]!!
-                val to = crates[cmd.to]!!
-                repeat(cmd.size) {
-                    to.push(from.poll()!!)
-                }
-            } else {
-                it.forEachIndexed { i, c ->
-                    if (c in 'A'..'Z') {
-                        crates.getOrPut((i + 1) / 4 + 1, ::LinkedList).add(c)
-                    }
-                }
-            }
+        val stacks = parseCrates(input)
+        parseCommands(input).forEach { (size, from, to) ->
+            move(stacks[from]!!, stacks[to]!!, size)
         }
-
-        return crates.values.map { it.peek() }.joinToString("")
+        return stacks.top()
     }
 
     override fun part2(input: Iterable<String>): Any {
-        return super.part2(input)
+        val stacks = parseCrates(input)
+        parseCommands(input).forEach { (size, from, to) ->
+            move2(stacks[from]!!, stacks[to]!!, size)
+        }
+        return stacks.top()
     }
 
-    fun String.parseCommand(): Command {
-        val chunks = split(' ')
-        return Command(chunks[1].toInt(), chunks[3].toInt(), chunks[5].toInt())
+    private fun move(from: Deque<Char>, to: Deque<Char>, size: Int) {
+        repeat(size) {
+            to.push(from.poll()!!)
+        }
     }
 
+    private fun move2(from: Deque<Char>, to: Deque<Char>, size: Int) {
+        LinkedList<Char>().apply {
+            repeat(size) { push(from.poll()) }
+            repeat(size) { to.push(poll()) }
+        }
+    }
 
     data class Command(
         val size: Int,
@@ -45,5 +43,37 @@ object Day05 : Task {
         val to: Int
     )
 
+
+    // --- util
+
+
+    private fun parseCommands(input: Iterable<String>): Sequence<Command> {
+        return input.asSequence()
+            .filter { it.startsWith('m') }
+            .map { it.toCommand() }
+    }
+
+    private fun parseCrates(input: Iterable<String>): SortedMap<Int, Deque<Char>> {
+        val crates = TreeMap<Int, Deque<Char>>()
+        input.takeWhile(String::isNotEmpty)
+            .forEach {
+                it.forEachIndexed { i, char ->
+                    if (char in 'A'..'Z') {
+                        crates.getOrPut((i + 1) / 4 + 1, ::LinkedList).add(char)
+                    }
+                }
+            }
+
+        return crates
+    }
+
+    private fun SortedMap<Int, Deque<Char>>.top(): String {
+        return values.map { it.peek() }.joinToString("")
+    }
+
+    private fun String.toCommand(): Command {
+        val chunks = split(' ')
+        return Command(chunks[1].toInt(), chunks[3].toInt(), chunks[5].toInt())
+    }
 
 }
